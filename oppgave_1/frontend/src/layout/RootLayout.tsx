89358172338b5b7c94e045fd72a028bd91ede489
arinpenahi <arinp@hiof.no>
2024-11-12 16:22:24 +0100

    Forandret innholdet på layout slik det inneholder footer og header. Fjernet det fra index.tsx

--- oppgave_1/frontend/src/layout/RootLayout.tsx
+++ oppgave_1/frontend/src/layout/RootLayout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Link from "next/link";
+import "../styles/css/main.css"
 
 // TODO: Kommenter ut om du ønsker å bruke .css
 
@@ -29,9 +31,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      {children}
-    </div>
+    <div
+        className="mx-auto grid min-h-screen w-full max-w-7xl grid-rows-[auto_minmax(900px,_1fr)_30px]"
+        data-testid="layout"
+      >
+        <nav className="mt-6 mb-12 flex justify-between">
+          <h1 className="text-lg font-bold uppercase" data-testid="logo">
+            <Link href="/">Mikro LMS</Link>{/*  Hovedside main */}
+          </h1> 
+          <ul className="flex gap-8" data-testid="nav">
+            <li className="text-base font-semibold" data-testid="nav_courses">
+            <Link href="/courses">Kurs</Link>{/*  Side kurs */}
+            </li>
+            <li className="text-base font-semibold" data-testid="nav_new">
+            <Link href="/course">Nytt kurs</Link>{/*  nytt kurs */}
+            </li>
+          </ul>
+        </nav>
+        <main className="h-full">
+          <p>Siden er tom</p>
+        </main>
+        <footer className="flex justify-between" data-testid="footer">
+          <p>Mikro LMS AS, 2024</p>
+          <p>99 00 00 00, mail@lms.no</p>
+        </footer>
+      </div>
   );
 }
 
--- oppgave_1/frontend/src/pages/index.tsx
+++ oppgave_1/frontend/src/pages/index.tsx
@@ -7,31 +7,7 @@ import Layout from '../layout/RootLayout';
 export default function All() {
     return (
       <Layout>
-      <div
-        className="mx-auto grid min-h-screen w-full max-w-7xl grid-rows-[auto_minmax(900px,_1fr)_30px]"
-        data-testid="layout"
-      >
-        <nav className="mt-6 mb-12 flex justify-between">
-          <h1 className="text-lg font-bold uppercase" data-testid="logo">
-            <Link href="/">Mikro LMS</Link>{/*  Hovedside main */}
-          </h1> 
-          <ul className="flex gap-8" data-testid="nav">
-            <li className="text-base font-semibold" data-testid="nav_courses">
-            <Link href="/courses">Kurs</Link>{/*  Side kurs */}
-            </li>
-            <li className="text-base font-semibold" data-testid="nav_new">
-            <Link href="/course">Nytt kurs</Link>{/*  nytt kurs */}
-            </li>
-          </ul>
-        </nav>
-        <main className="h-full">
-          <p>Siden er tom</p>
-        </main>
-        <footer className="flex justify-between" data-testid="footer">
-          <p>Mikro LMS AS, 2024</p>
-          <p>99 00 00 00, mail@lms.no</p>
-        </footer>
-      </div>
+      {/*innhold signup?*/ }
       </Layout>
     );
   }
